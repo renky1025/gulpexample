9f9932e7f92b9aa5e22b44eea2b8a35cc3f2212e
Kangyao Ren <kangyao.ren@autodesk.com>
2016-01-29 10:58:30 +0800

    again

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp'),
+browserSync = require('browser-sync'),
+less = require('gulp-less'),
+cssmin = require('gulp-minify-css'),
+notify = require('gulp-notify'),
+plumber = require('gulp-plumber'),
+useref = require('gulp-useref'),
+uglify = require('gulp-uglify'),
+gulpIf = require('gulp-if'),
+cache = require('gulp-cache'),
+cssnano = require('gulp-cssnano'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+runSequence = require('run-sequence');
+
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf(['*.js', '!*.min.js'], uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+//	Copying Fonts to Dist
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('compileLess', function () {
+    return gulp.src('app/less/*.less')
+    	.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(less())
+        .pipe(gulp.dest('app/css'))
+	    .pipe(browserSync.reload({
+	      stream: true
+	    }));
+});
+
+gulp.task('changesWatch', ['browserSync', 'compileLess'], function () {
+    gulp.watch('app/less/*.less', ['compileLess']); //当所有less文件发生改变时，调用testLess任务
+	// Reloads the browser whenever HTML or JS files change
+	gulp.watch('app/*.html', browserSync.reload); 
+	gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['compileLess', 'useref', 'images', 'fonts'],
+    callback
+  )
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['compileLess','browserSync', 'changesWatch'],
+    callback
+  )
+});
